a224a228202b5aec1e696efd8649a3b10db57f85
DavidDavilaEID <david.davila@electronicid.eu>
2018-12-11 15:31:26 +0100

    da

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 // These are important and needed before anything else
 import 'zone.js/dist/zone-node';
+
 import 'reflect-metadata';
 
 import { renderModuleFactory } from '@angular/platform-server';
