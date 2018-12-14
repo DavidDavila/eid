ae19e2d3dc78b60d5862c26c5e560eb0b8bdf85b
DavidDavilaEID <david.davila@electronicid.eu>
2018-12-14 11:34:44 +0100

    da

--- src/app/home/presentation/slider/slider.component.ts
+++ src/app/home/presentation/slider/slider.component.ts
@@ -4,23 +4,7 @@ import {animate, query, stagger, style, transition, trigger} from '@angular/anim
 @Component({
   selector: '[app-slider]',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss'],
-    animations: [
-      trigger('parentAnimation', [
-                  transition(':enter', [
-                      query('.child', style({transform:'translateY(4vh)',opacity: 0})),
-                      query('.child', stagger('200ms', [
-                          animate('500ms 650ms ease-out', style({transform:'translateY(0vh)',opacity: 1}))
-                      ]))
-                  ]),
-                  transition(':leave', [
-                      query('.child', style({transform:'translateY(0vh)',opacity: 1})),
-                      query('.child', stagger('100ms', [
-                          animate('500ms 1ms ease-out', style({transform:'translateY(-14vh)',opacity: 0}))
-                      ]))
-                  ])
-              ])
-    ]
+  styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
   @Input() slide: number;
