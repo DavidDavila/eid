df3a886ebad404cbca265e16b4afddff993a023b
Cris-eID <cristina.rojo@electronicid.eu>
2019-04-02 17:20:01 +0200

    smileid

---
+++ src/app/solutions/smile-id/circle-img/circle-img.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-circle-img',
+  templateUrl: './circle-img.component.html',
+  styleUrls: ['./circle-img.component.scss']
+})
+export class CircleImgComponent implements OnInit {
+
+  @Input() data;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/solutions/smile-id/smile-id.component.ts
+++ src/app/solutions/smile-id/smile-id.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
+import { CircleImgComponent } from './circle-img/circle-img.component';
+
 @Component({
   selector: 'app-smile-id',
   templateUrl: './smile-id.component.html',
@@ -7,16 +9,61 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class SmileIdComponent implements OnInit {
   public section:number = 0;
+  public circles: any[] = [
+        {
+          "img": "access.png",
+          "text": "Access buildings",
+          "class": "withMargin",
+          "classImg": "one"
+        },
+        {
+          "img": "web.png",
+          "text": "Web",
+          "class": "withMargin",
+          "classImg": "two"
+        },
+        {
+          "img": "mobile.png",
+          "text": "Native mobile",
+          "class": "withMargin",
+          "classImg": "three"
+        },
+        {
+          "img": "atm.png",
+          "text": "ATM",          
+          "class": "withMargin",
+          "classImg": "four"
+        },
+        {
+          "img": "cctv.png",
+          "text": "CCTV",
+          "classImg": "five"
+        }
+      ];
+
+  public lottieConfig: Object = {};
+
   @ViewChild('smileId') smileId: ElementRef;
 
-
-
   constructor() {
-
   }
 
   ngOnInit() {
 
+  }
+
+  createLottie(name:string) {
+    return this.lottieConfig = {
+        path: '/assets/json/' + name + '.json',
+        renderer: 'svg',
+        autoplay: false,
+        loop: false,
+    };
+  }
+  handleAnimation(anim: any, delay: number) {
+    console.log(anim)
+    anim.setSpeed(1.5);
+    setTimeout(()=>{anim.play()}, delay);
   }
 
   mouseFinish(event, reverse){
@@ -25,6 +72,8 @@ export class SmileIdComponent implements OnInit {
     } else {
     this.section-- ;
     }
+
+    console.log('section: ', this.section)
     let currentElement = this.smileId.nativeElement.children[this.section];
     this.smileId.nativeElement.scroll({ top: currentElement.offsetTop, behavior: 'smooth' })
   }
