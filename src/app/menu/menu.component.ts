ad231f896cfed9bbe7d889ffcbb113d1eee8455e
Cris-eID <cristina.rojo@electronicid.eu>
2019-04-22 16:28:44 +0200

    ok

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,12 +7,19 @@ import { ScrollService } from '../shared/services/scroll.service'
 })
 export class MenuComponent implements OnInit {
   public scrollZero: boolean = true;
+  public wasClicked: boolean = false;
   constructor(private _scrollService: ScrollService) { }
 
   ngOnInit() {
     this._scrollService.section$.subscribe( val=>{
       this.scrollZero = val;
+      this.scrollZero && (this.wasClicked = false)
     })
+  }
+
+
+  onClick() {
+      this.wasClicked = !this.wasClicked;
   }
 
 }
