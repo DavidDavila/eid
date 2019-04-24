da65a72c5dfc685dc75680d1696d56ef66c6318b
Cris-eID <cristina.rojo@electronicid.eu>
2019-04-24 10:32:52 +0200

    ok

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,19 +8,26 @@ import { ScrollService } from '../shared/services/scroll.service'
 export class MenuComponent implements OnInit {
   public scrollZero: boolean = true;
   public wasClicked: boolean = false;
+  public showSubmenu : boolean = false;
   constructor(private _scrollService: ScrollService) { }
 
   ngOnInit() {
     this._scrollService.section$.subscribe( val=>{
       this.scrollZero = val;
-      this.wasClicked = false
+      this.wasClicked = false;
+      this.showSubmenu = false;
       // this.scrollZero && (this.wasClicked = false)
     })
   }
 
-
   onClick() {
-      this.wasClicked = !this.wasClicked;
+    this.wasClicked = !this.wasClicked;
+    this.showSubmenu = false;
+  }
+
+  mouseOver(event){
+    this.showSubmenu = (event.target.className == 'show') ? true : false;
+    console.log('mouseOver -- ', this.showSubmenu );
   }
 
 }
