573e71539f226cfbbe3ffd3903069f9e08bfc15f
Cris-eID <cristina.rojo@electronicid.eu>
2019-05-08 12:49:38 +0200

    opacity animation leave lotties home

--- src/app/home/features/features.component.ts
+++ src/app/home/features/features.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from './../../shared/services/home.service';
 
+import {trigger, state, style, animate, transition} from '@angular/animations';
+
 @Component({
   selector: '[app-features]',
   templateUrl: './features.component.html',
-  styleUrls: ['./features.component.scss']
+  styleUrls: ['./features.component.scss'],
+  animations: [
+    trigger('showHide', [
+      transition(':enter', [
+        style({ opacity: 1 }),
+        animate(500)
+      ]),
+      transition(':leave', [
+        animate(500, style({ opacity: 0 }))
+      ]),
+    ])
+  ],
 })
 export class FeaturesComponent implements OnInit {
 	public section:number;
-    public lottieConfig: Object = {};
+  public lottieConfig: Object = {};
+ 
   constructor(
 		private _homeService: HomeService ) {
 		this._homeService.section$.subscribe( val=>{
