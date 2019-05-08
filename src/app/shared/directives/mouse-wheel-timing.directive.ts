894e2d50a0926f761ab501f4debecee75721cfa2
DavidDavilaEID <david.davila@electronicid.eu>
2019-05-08 13:34:58 +0200

    fixed

--- src/app/shared/directives/mouse-wheel-timing.directive.ts
+++ src/app/shared/directives/mouse-wheel-timing.directive.ts
@@ -1,27 +1,27 @@
-import { Directive, Output, EventEmitter,Input, HostListener, ElementRef, OnDestroy } from '@angular/core';
+import { Directive, Output, EventEmitter, Input, HostListener, ElementRef, OnDestroy } from '@angular/core';
 import { Observable, fromEvent } from 'rxjs';
 @Directive({
   selector: '[mouseWheelTiming]'
 })
 export class MouseWheelTimingDirective {
-  @Input('disableScroll') disableScroll:boolean;
+  @Input('disableScroll') disableScroll: boolean;
   @Input('timeToBlock') timeToBlock: number;
   @Input('propagation') propagation: number;
-  
+
   @Output() mouseFinish: EventEmitter<number> = new EventEmitter<number>();
-  private isMoving:boolean;
+  private isMoving: boolean;
   private scrollEvent$;
   constructor(private el: ElementRef) {
     this.scrollEvent$ = fromEvent(this.el.nativeElement,
     'mousewheel').subscribe((e: any) => {
-      if(!this.propagation) {
+      if (!this.propagation) {
         e.stopPropagation();
         e.preventDefault();
       }
       if (this.isMoving || this.disableScroll) { return; }
       this.mouseFinish.emit(e);
       this.isMoving = true;
-      setTimeout((()=> {
+      setTimeout((() => {
          this.isMoving = false;
       }).bind(this), this.timeToBlock || 1200);
     });
@@ -30,10 +30,10 @@ export class MouseWheelTimingDirective {
     'DOMMouseScroll').subscribe((e: any) => {
       e.stopPropagation();
       e.preventDefault();
-      if (this.isMoving) return;
+      if (this.isMoving) { return; }
       this.mouseFinish.emit(e);
       this.isMoving = true;
-      setTimeout((()=> {
+      setTimeout((() => {
          this.isMoving = false;
       }).bind(this), this.timeToBlock || 1200);
     });
