5ddc238503b54d106cb7e6f9ffeac66ac6dab8bf
Cris-eID <cristina.rojo@electronicid.eu>
2019-05-08 10:42:16 +0200

    close menu on click inner element

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ScrollService } from '../shared/services/scroll.service'
+import { Component, OnInit, HostListener, Input, ElementRef } from '@angular/core';
+import { ScrollService } from '../shared/services/scroll.service';
+import {Router} from "@angular/router";
+
 @Component({
   selector: '[app-menu]',
   templateUrl: './menu.component.html',
@@ -8,15 +10,26 @@ import { ScrollService } from '../shared/services/scroll.service'
 export class MenuComponent implements OnInit {
   public scrollZero: boolean = true;
   public wasClicked: boolean = false;
-  public showSubmenu : boolean = false;
-  constructor(private _scrollService: ScrollService) { }
+  public showSubmenu: boolean = false;
+
+  @HostListener('document:click', ['$event']) 
+  clickout(event) {
+    if( !this.eRef.nativeElement.contains(event.target) && this.wasClicked) {
+      this.onClick();
+    } 
+  }
+
+  constructor(
+    private router: Router,
+    private _scrollService: ScrollService, 
+    private eRef: ElementRef
+  ) { }
 
   ngOnInit() {
     this._scrollService.section$.subscribe( val=>{
       this.scrollZero = val;
       this.wasClicked = false;
       this.showSubmenu = false;
- 
       // this.scrollZero && (this.wasClicked = false)
     })
   }
@@ -28,11 +41,17 @@ export class MenuComponent implements OnInit {
 
   mouseOver(event){
     this.showSubmenu = (event.target.className == 'show') ? true : false;
-    console.log('mouseOver -- ', this.showSubmenu );
+    console.log('mouseOver -- ', this.showSubmenu);
   }
+
 
   closeSubmenu(){
     this.showSubmenu = false;
   }
 
+  closeAndGo(ev){
+    this.onClick();
+    let goTo = ev.currentTarget.getAttribute('href');
+    this.router.navigate([goTo]);
+  }
 }
